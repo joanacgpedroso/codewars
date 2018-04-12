52cd3475d2863a3061135060db1db0fae700021a
Joana Pedroso <26660640+joanacgpedroso@users.noreply.github.com>
2018-04-12 23:08:52 +0100

    Update Counting Duplicates kata

--- 6-kyu/counting-duplicates.js
+++ 6-kyu/counting-duplicates.js
@@ -1,28 +1,3 @@
-function duplicateCount (text) {
-  let count = 0
-  text = text.toLowerCase()
-
-  // Iterate through all characters of text
-  for (let i = 0; i < text.length; i++) {
-    // Choose one character
-    let firstCharacterToCompare = text.charAt(i)
-
-    // Iterate through all characters of text
-    for (let j = 0; j < text.length; j++) {
-      let secondCharacterToCompare = text.charAt(j)
-      // Check if same character
-      if ((i !== j) && (firstCharacterToCompare === secondCharacterToCompare)) {
-        count++
-        // Replace all characters equal to compared character in text so it's not compared again
-        text = text.replace(new RegExp(firstCharacterToCompare, 'g'), '')
-        j++
-        break
-      }
-    }
-  }
-  return count
-}
-
 function duplicateCountSolution (text) {
   let count = 0
   text = text.toLowerCase()
@@ -50,4 +25,52 @@ function duplicateCountSolution (text) {
 
   }
   return count
+}
+
+function duplicateCount2 (text) {
+  let count = 0
+
+  text
+    .toLowerCase()
+    .split('')
+    .forEach((letter, index, array) => {
+      console.log('Letter: ', letter)
+      console.log('Index: ', index)
+      console.log('Array: ', array)
+
+      array.forEach((l) => {
+        console.log((l === letter) && (index !== array.indexOf(l)))
+      })
+    })
+}
+
+function duplicateCount3 () {
+    let lettersAlreadyCompared = []
+    text
+      .toLowerCase()
+      .split('')
+      .forEach((letter, index, array) => {
+        console.log('Array: ', array)
+  
+        array.some((l) => {
+          //console.log((l === letter) && (index !== array.indexOf(l)))
+          console.log('Letters already compared: ', lettersAlreadyCompared)      
+          console.log('-Letter: ', letter, l, l === letter)
+          //console.log('-Index: ', index, array.indexOf(l), index === array.indexOf(l))
+          console.log((letter !== lettersAlreadyCompared[i]))
+          for (var i = 0; i < lettersAlreadyCompared.length; i++) {
+          lettersAlreadyCompared.push(letter)
+            if ((letter !== lettersAlreadyCompared[i]) && (l === letter)) {
+               lettersAlreadyCompared.push(letter) 
+            } else {
+              console.log('skip up')
+              lettersAlreadyCompared.push(letter)
+              return true
+            }
+            lettersAlreadyCompared.push(letter)
+          }
+          lettersAlreadyCompared.push(letter)
+          //if (index === array.indexOf(l)) return true
+        })
+      })
 }
